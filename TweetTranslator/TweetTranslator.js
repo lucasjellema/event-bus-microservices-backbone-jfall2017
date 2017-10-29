c521c1c50ba756e02cec64516b35ce0a20bcb5fe
Lucas Jellema <lucasjellema@gmaIL.com>
2017-10-29 15:08:58 +0100

    add log statements to TweetTranslator

--- TweetTranslator/TweetTranslator.js
+++ TweetTranslator/TweetTranslator.js
@@ -2,8 +2,8 @@ var http = require('http'),
   request = require('request'),
   express = require('express'),
   bodyParser = require('body-parser')
-  translateGoogle = require('google-translate-api');
-  ;
+translateGoogle = require('google-translate-api');
+;
 
 //var localCacheAPI = require("./local-cache-api.js");
 var localLoggerAPI = require("./local-logger-api.js");
@@ -47,35 +47,36 @@ app.get('/ping', function (req, res) {
 });
 
 app.post('/tweet', function (req, res) {
-  return new Promise(function(resolve,reject) {
-  // Get the key and value
+  return new Promise(function (resolve, reject) {
+    // Get the key and value
 
-  console.log('TweetTranslator - translate tweet');
-  console.log('body in request' + JSON.stringify(req.body));
-  console.log("content type " + req.headers['content-type']);
-  var tweet = req.body;
-  translate(tweet).then((translatedTweet) =>
-     {  var responseBody = { "translatedTweet": translatedTweet };
-        // Send the response
-        res.setHeader('Content-Type', 'application/json');
-        res.send(responseBody);
-     } ) 
-     resolve();
-    })// Promise
+    console.log('TweetTranslator - translate tweet');
+    console.log('body in request' + JSON.stringify(req.body));
+    console.log("content type " + req.headers['content-type']);
+    var tweet = req.body;
+    translate(tweet).then((translatedTweet) => {
+      var responseBody = { "translatedTweet": translatedTweet };
+      // Send the response
+      res.setHeader('Content-Type', 'application/json');
+      res.send(responseBody);
+    })
+    resolve();
+  })// Promise
 });
 
 function translate(translatingTweet) {
-  return new Promise( (resolve, reject) => { 
-  translatingTweet.translations = [];
-  Promise.all([
-      translateGoogle(translatingTweet.text, {to: 'de'})
-    , translateGoogle(translatingTweet.text, {to: 'es'})
-    , translateGoogle(translatingTweet.text, {to: 'fr'})
-    , translateGoogle(translatingTweet.text, {from:"en",to: 'nl'})
-  ]).then( (translations)=> {
-    translations.forEach( (val) => {translatingTweet.translations.push(val.text)} );
-    resolve (translatingTweet);
-  }) //then
+
+  return new Promise((resolve, reject) => {
+    translatingTweet.translations = [];
+    Promise.all([
+      translateGoogle(translatingTweet.text, { to: 'de' })
+      , translateGoogle(translatingTweet.text, { to: 'es' })
+      , translateGoogle(translatingTweet.text, { to: 'fr' })
+      , translateGoogle(translatingTweet.text, { from: "en", to: 'nl' })
+    ]).then((translations) => {
+      translations.forEach((val) => { translatingTweet.translations.push(val.text) });
+      resolve(translatingTweet);
+    }) //then
   })//promise
 }//translate
 
@@ -87,57 +88,79 @@ function handleWorkflowEvent(eventMessage) {
   var event = JSON.parse(eventMessage.value);
   console.log("received message", eventMessage);
   console.log("actual event: " + JSON.stringify(event));
+  localLoggerAPI.log("handleWorkflowEvent :" + event.workflowConversationIdentifier + ")"
+    , APP_NAME, "info");
 
   // event we expect is of type workflowEvents
   // we should do something with this event if it contains an action (actions[].type=TweetTranslatorActionType where status ="new" and conditions are satisfied)
-
-  if (event.actions) {
-    var acted = false;
-    for (i = 0; i < event.actions.length; i++) {
-      var action = event.actions[i];
-      // find action of type TranslateTweet
-      if (TweetTranslatorActionType == action.type) {
-        // check conditions
-        if ("new" == action.status
-          && conditionsSatisfied(action, event.actions)) {
-          var workflowDocument;
-          localCacheAPI.getFromCache(event.workflowConversationIdentifier, function (document) {
-            console.log("Workflow document retrieved from cache");
-            var workflowDocument = document;
-            // this happens  asynchronously; right now we do not actually use the retrieved document. It does work.       
-          });
-          // if satisfied, then translate tweet
-          translate(event.payload).then((translatedTweet) => {  
-            event.payload = translatedTweet;
-            // update action in event
-            action.status = 'complete';
-            action.result = 'OK';
-            // add audit line
-            event.audit.push(
-              { "when": new Date().getTime(), "who": "TweetTranslator", "what": "update", "comment": "Tweet Translation Performed" }
-            );
-            acted = true;           
-            if (acted) {
-              event.updateTimeStamp = new Date().getTime();
-              event.lastUpdater = APP_NAME;
-              // publish event
-              eventBusPublisher.publishEvent('OracleCodeTwitterWorkflow' + event.updateTimeStamp,event, workflowEventsTopic);
-        
-              localLoggerAPI.log("Translated Tweet  - (workflowConversationIdentifier:" + event.workflowConversationIdentifier + ")"
+  try {
+    if (event.actions) {
+      var acted = false;
+      for (i = 0; i < event.actions.length; i++) {
+        var action = event.actions[i];
+        // find action of type TranslateTweet
+        if (TweetTranslatorActionType == action.type) {
+          localLoggerAPI.log("handleWorkflowEvent : action type was found " + action.type
+            , APP_NAME, "info");
+          // check conditions
+          if ("new" == action.status
+            && conditionsSatisfied(action, event.actions)) {
+            localLoggerAPI.log("handleWorkflowEvent : conditions Satisfied "
+              , APP_NAME, "info");
+            var workflowDocument;
+            localCacheAPI.getFromCache(event.workflowConversationIdentifier, function (document) {
+              localLoggerAPI.log("handleWorkflowEvent : workflow slip from cache " + JSON.stringify(document)
                 , APP_NAME, "info");
-        
-              // PUT Workflow Document back  in Cache under workflow event identifier
-              localCacheAPI.putInCache(event.workflowConversationIdentifier, event,
-                function (result) {
-                  console.log("store workflowevent plus routing slip in cache under key " + event.workflowConversationIdentifier + ": " + JSON.stringify(result));
-                });
-            }// acted
-          })// then
-        }
-      }// if TranslateTweet
-      // if any action performed, then republish workflow event and store routingslip in cache
-    }//for
-  }// if actions
+              console.log("Workflow document retrieved from cache");
+              var workflowDocument = document;
+              // this happens  asynchronously; right now we do not actually use the retrieved document. It does work.       
+            });
+            // if satisfied, then translate tweet
+            localLoggerAPI.log("handleWorkflowEvent : go enter translate promise "
+            , APP_NAME, "info");
+
+            translate(event.payload).then((translatedTweet) => {
+              localLoggerAPI.log("handleWorkflowEvent : then translated " + JSON.stringify(translatedTweet)
+              , APP_NAME, "info");
+  
+              event.payload = translatedTweet;
+              // update action in event
+              action.status = 'complete';
+              action.result = 'OK';
+              // add audit line
+              localLoggerAPI.log("handleWorkflowEvent : push to audit"
+              , APP_NAME, "info");
+  
+              event.audit.push(
+                { "when": new Date().getTime(), "who": "TweetTranslator", "what": "update", "comment": "Tweet Translation Performed" }
+              );
+              acted = true;
+              if (acted) {
+                event.updateTimeStamp = new Date().getTime();
+                event.lastUpdater = APP_NAME;
+                // publish event
+                eventBusPublisher.publishEvent('OracleCodeTwitterWorkflow' + event.updateTimeStamp, event, workflowEventsTopic);
+
+                localLoggerAPI.log("Translated Tweet  - (workflowConversationIdentifier:" + event.workflowConversationIdentifier + ")"
+                  , APP_NAME, "info");
+
+                // PUT Workflow Document back  in Cache under workflow event identifier
+                localCacheAPI.putInCache(event.workflowConversationIdentifier, event,
+                  function (result) {
+                    console.log("store workflowevent plus routing slip in cache under key " + event.workflowConversationIdentifier + ": " + JSON.stringify(result));
+                  });
+              }// acted
+            })// then
+          }
+        }// if TranslateTweet
+        // if any action performed, then republish workflow event and store routingslip in cache
+      }//for
+    }// if actions
+  } catch (err) {
+    localLoggerAPI.log("handleWorkflowEvent : EXCEPTION " + err
+      , APP_NAME, "info");
+
+  }
 }// handleWorkflowEvent
 
 function conditionsSatisfied(action, actions) {
